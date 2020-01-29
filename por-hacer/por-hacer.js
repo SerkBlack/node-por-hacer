e90bde83aae8425b77d6de3bdd6eb25476280818
Sergio <sergio.zapata@nivelics.co>
2020-01-29 15:10:51 -0500

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,108 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+    fs.writeFile('db/data.json/', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+
+
+    })
+}
+
+const cargarDB = () => {
+
+    try {
+
+        listadoPorHacer = require('../db/data.json');
+
+    } catch (error) {
+        listadoPorHacer = []
+    }
+
+
+
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    console.log(index);
+
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+
+    let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion
+    });
+
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+    // let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    // console.log(listadoPorHacer);
+    // console.log(descripcion);
+
+    // console.log(index);
+
+    // if (index >= 0) {
+    //     delete listadoPorHacer[index];
+    //     console.log(listadoPorHacer);
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
